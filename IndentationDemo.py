31760f7fd53436e1644cdd2cd76fc7f2208239c8
Lakhan Singh <lakhans671@gmail.com>
2020-08-06 13:19:30 +0530

    python core

---
+++ IndentationDemo.py
@@ -0,0 +1,5 @@
+if(2>1 and 4>2):
+    print("4 space...")
+if(1 or 1>2):
+    print("4 space...")
+print("Rest of the code")
---
+++ LogicalOperator.py
@@ -0,0 +1,5 @@
+if(2>1 and 4>2):
+    print("1st logical condition")
+if(1 or 1>2):
+    print("2nd logical condition")
+print("Rest of the code")
