33347de78f5607cce4ff1b5bea4c69092156392d
Lakhan Singh <lakhans671@gmail.com>
2020-08-06 22:30:37 +0530

    python core

---
+++ RangeFunction.py
@@ -0,0 +1,12 @@
+for x in range(1,10,1):
+    print(x)
+
+for x in range( 10):
+        print(x)
+
+
+print(range(3,10,3)[2])
+print("#############################")
+for y in range(5,0,-1):{
+    print(y)
+}
